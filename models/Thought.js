d5b3fd5b94ec883246835ea7cda2dc480d0090b2
Kyle <kferguson@captechconsulting.com>
2021-11-30 14:57:51 -0600

    building the structure for project. Need to now refactor the code itself from module work

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
+
+
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    // THIS line is associated the comments model to this parents pizza model
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
+
+// get total count of comments and replies on retrieval
+// Here we're using the .reduce() method to tally up the total of every comment with its replies. In its basic form, .reduce() takes two parameters, an accumulator and a currentValue. Here, the accumulator is total, and the currentValue is comment. As .reduce() walks through the array, it passes the accumulating total and the current value of comment into the function, with the return of the function revising the total for the next iteration through the array.
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
+// export the Pizza model
+module.exports = Pizza;
